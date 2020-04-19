e6342fd947a0c8661a1dd24b588058fe861f63db
Rick <ricktang1991@gmail.com>
2020-04-19 14:35:40 -0700

    First commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by 桑染 on 2020-04-18.
+//  Copyright © 2020 Rick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
